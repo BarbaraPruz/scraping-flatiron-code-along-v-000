7fa05c7dbbead67858f39ce85ae36f8138f8f907
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-09 19:09:29 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,16 @@
 class Course
+  attr_accessor :title, :description, :schedule
 
+  @@ all 
+  
+  def initialize 
+    @@all << self
+  end
+
+  def self.all 
+    @@all 
+  end
+  def self.reest_all
+    @@all.clear
+  end
 end
