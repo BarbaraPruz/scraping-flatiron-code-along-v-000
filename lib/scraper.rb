4e5048ee9416c23484d3dc4adb1bf037f87965a4
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-09 20:10:03 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,13 +15,19 @@ class Scraper
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
-    binding.pry
   end
 
   def get_courses
+    self.get_page.css(".post")
   end
 
   def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
 
   def print_courses
@@ -36,5 +42,3 @@ class Scraper
   end
 
 end
-
-Scraper.new.get_page
